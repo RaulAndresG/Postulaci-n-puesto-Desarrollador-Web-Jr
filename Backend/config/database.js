b77deab2333c0cf893a822d6d12193b16015c7c8
RaulAndresG <reaulgomes26@gmail.com>
2024-03-06 20:01:45 -0500

    Avance🎨

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -1,16 +1,20 @@
-const mongoose = require('mongoose');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
 
-const conectarMdb = async () => {
+const MONGO_URI = process.env.MONGO_URI;
+const dbName = process.env.DB_NAME;
+
+async function conectarMdb() {
     try {
-        await mongoose.connect(process.env.MONGO_URI/* , { 
-            useNewUrlParser: true, 
-            useUnifiedTopology: true 
-        } */);
-        console.log('MongoDB conectado');
+        const client = new MongoClient(MONGO_URI/* , { useNewUrlParser: true, useUnifiedTopology: true } */);
+        await client.connect();
+        console.log("MongoDB conectado");
+        return client.db(dbName);
     } catch (error) {
-        console.error('Error al conectar a MongoDB:', error.message);
-        process.exit(1);
+        console.error("Error al conectar a MongoDB:", error);
+        throw error;
     }
 }
 
 module.exports = conectarMdb;
+
--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -1,10 +1,11 @@
+//**DEPENDENCIAS Y VARIABLES GLOBALES**
+
 const express = require('express');
 const router = express.Router();
 const { MongoClient, ObjectId } = require('mongodb');
 const moment = require('moment');
-
+const conectarMdb = require('../config/database');
 require('dotenv').config();
-
 const dbURI = process.env.MONGO_URI;
 const dbName = process.env.DB_NAME;
 const pacientesCollection = 'pacientes';
@@ -30,6 +31,10 @@ router.get('/pacientes', async (req, res) => {
     }
 });
 
+
+
+// **PACIENTES**
+
 // Crear un nuevo paciente
 router.post('/pacientes', async (req, res) => {
     try {
@@ -43,6 +48,34 @@ router.post('/pacientes', async (req, res) => {
         res.status(500).json({ error: "Error al crear paciente" });
     }
 });
+// Actualizar un paciente por su ID
+router.put('/pacientes/:id', async (req, res) => {
+    try {
+        const db = await connectToDatabase();
+        const collection = db.collection(pacientesCollection);
+        const result = await collection.updateOne({ _id: new ObjectId(req.params.id) }, { $set: req.body });
+        res.json({ message: "Paciente actualizado exitosamente", data: result });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Error al actualizar paciente" });
+    }
+});
+// Eliminar un paciente por su ID
+router.delete('/pacientes/:id', async (req, res) => {
+    try {
+        const db = await connectToDatabase();
+        const collection = db.collection(pacientesCollection);
+        const result = await collection.deleteOne({ _id: new ObjectId(req.params.id) });
+        res.json({ message: "Paciente eliminado exitosamente", data: result });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Error al eliminar paciente" });
+    }
+});
+
+
+
+// **HISTORIAS CLINICAS**
 
 // Obtener todas las historias clínicas
 router.get('/historiasclinicas', async (req, res) => {
@@ -57,56 +90,53 @@ router.get('/historiasclinicas', async (req, res) => {
     }
 });
 
-// Crear una nueva historia clínica
-router.post('/historiasclinicas', async (req, res) => {
+// Función para generar el subserial
+async function generarSubserial(pacienteId) {
     try {
-        const db = await connectToDatabase();
-        const collection = db.collection(historiasClinicasCollection);
-        const result = await collection.insertOne(req.body);
-        res.status(201).json({ message: "Historia Clinica creado exitosamente", data: result.ops ? result.ops[0] : null });
+        const db = await conectarMdb();
+        if (!db) {
+            throw new Error("No se pudo conectar a la base de datos");
+        }
+
+        const collection = db.collection('historias_clinicas');
+        const numeroHistoriasClinicas = await collection.countDocuments({ paciente_id: pacienteId }) + 1;
+        return `${pacienteId}-${numeroHistoriasClinicas}`;
     } catch (error) {
-        console.error(error);
+        console.error("Error al generar subserial:", error);
+        throw error;
+    }
+}
+
+// Crear una nueva historia clínica
+router.post('/historiasclinicas/serial', async (req, res) => {
+    try {
+        const pacienteId = req.body.paciente_id;
+        const subserial = await generarSubserial(pacienteId);
+        // Agregar el subserial a los datos de la historia clínica antes de guardarla en la base de datos
+        req.body.subserial = subserial;
+        const db = await conectarMdb();
+        if (!db) {
+            throw new Error("No se pudo conectar a la base de datos");
+        }
+        const collection = db.collection('historias_clinicas');
+        const result = await collection.insertOne(req.body);
+        res.status(201).json({ message: "Historia clínica creada exitosamente", data: result.ops ? result.ops[0] :null });
+    } catch (error) {
+        console.error("Error al crear historia clínica:", error);
         res.status(500).json({ error: "Error al crear historia clínica" });
     }
 });
-
-// Actualizar un paciente por su ID
-router.put('/pacientes/:id', async (req, res) => {
-    try {
-        const db = await connectToDatabase();
-        const collection = db.collection(pacientesCollection);
-        const result = await collection.updateOne({ _id: new ObjectId(req.params.id) }, { $set: req.body });
-        res.json({ message: "Paciente actualizado exitosamente", data: result });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Error al actualizar paciente" });
-    }
-});
-
 
 // Actualizar una historia clínica por su ID
 router.put('/historiasclinicas/:id', async (req, res) => {
     try {
         const db = await connectToDatabase();
         const collection = db.collection(historiasClinicasCollection);
-        const result = await collection.updateOne({ _id: ObjectId(req.params.id) }, { $set: req.body });
+        const result = await collection.updateOne({ _id: new ObjectId(req.params.id) }, { $set: req.body });
         res.json({ message: "Historia clínica actualizada exitosamente", data: result });
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: "Error al actualizar historia clínica" });
-    }
-});
-
-// Eliminar un paciente por su ID
-router.delete('/pacientes/:id', async (req, res) => {
-    try {
-        const db = await connectToDatabase();
-        const collection = db.collection(pacientesCollection);
-        const result = await collection.deleteOne({ _id: ObjectId(req.params.id) });
-        res.json({ message: "Paciente eliminado exitosamente", data: result });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Error al eliminar paciente" });
     }
 });
 
@@ -115,7 +145,7 @@ router.delete('/historiasclinicas/:id', async (req, res) => {
     try {
         const db = await connectToDatabase();
         const collection = db.collection(historiasClinicasCollection);
-        const result = await collection.deleteOne({ _id: ObjectId(req.params.id) });
+        const result = await collection.deleteOne({ _id: new ObjectId(req.params.id) });
         res.json({ message: "Historia clínica eliminada exitosamente", data: result });
     } catch (error) {
         console.error(error);
